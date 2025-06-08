0336fddce2766489e0667824dffcab9ae229d5b9
hayong39 <yia0524_hy@naver.com>
2025-06-09 01:32:12 +0900

    chore: 불필요한 코드 주석 처리 및 auth.js 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Login from "./pages/Login";
 import KakaoCallback from "./pages/KakaoCallback";
-import "./utils/auth";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/kakaocallback" element={<KakaoCallback />} />
         </Routes>
--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
