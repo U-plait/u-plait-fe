c6d658a3b478be4095acafad5053b60816255786
yoon_je <etoile0626@naver.com>
2025-06-13 11:02:02 +0900

    refactor: css 파일 위치 변경

--- src/pages/MyReviews.jsx
+++ src/pages/MyReviews.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './MyReviews.css';
+import '../styles/MyReviews.css';
 
 const MyReviews = () => {
     const [reviews, setReviews] = useState([]);
