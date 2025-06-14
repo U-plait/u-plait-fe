1f23b5ff16feadd0f978d05799a2a960493e6680
etoile0626 <etoile0626@naver.com>
2025-06-14 20:29:14 +0900

    feat: 마이페이지 개인정보 수정 화면 구현 중

---
+++ src/pages/MypageEdit.jsx
@@ -0,0 +1,9 @@
+import {useEffect, useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import "../styles/MypageEdit.css";
+
+const MypageEdit = () => {
+
+
+export default MypageEdit;
